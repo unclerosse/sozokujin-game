5d280424d7494ad88e2f4586b4f5900d0b09e940
GrigoryAristov <grigory.ar@gmail.com>
2023-04-26 21:39:56 +0300

    add some animations

--- player.gd
+++ player.gd
@@ -4,24 +4,31 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -700.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var vel = Vector2()
 
+
 func _physics_process(delta):
-	#Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta*2
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var direction = Input.get_axis("player_left", "player_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	animate()
+	
+func animate():
+	if velocity.x == 0:
+		$Sprite.play("idle")
+	elif velocity.y == 0 and $RayCast2D.is_colliding() and velocity.x != 0:
+		$Sprite.play("run")
+		if velocity.x > 0:
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true

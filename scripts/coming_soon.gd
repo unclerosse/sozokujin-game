900ee9e1c820089c092422d2a39c075c53c401c3
GrigoryAristov <grigory.ar@gmail.com>
2023-05-29 19:56:28 +0300

    remade map

--- scripts/coming_soon.gd
+++ scripts/coming_soon.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 
 func _on_button_2_pressed():
